d0eca37270a6e9868f1268aa7e16815427c8479e
mamenz752 <mamenz752@gmail.com>
2024-03-20 11:13:14 +0900

    fix: headerコンポーネントのフォルダ構成変更

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="">
+      <div>
+        <img src="" alt="logo" />
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
