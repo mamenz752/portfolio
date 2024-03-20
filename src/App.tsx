76c1fb233d9533e4ff89e17e60bb71f115e22969
mamenz752 <mamenz752@gmail.com>
2024-03-20 11:28:07 +0900

    feat: react-router-domの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AboutPresenter } from "./components/about/components/AboutPresenter";
+import { CareerPresenter } from "./components/career/components/CareerPresenter";
+import { ContactPresenter } from "./components/contact/components/ContactPresenter";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/about" element={<AboutPresenter />} />
+          <Route path="/career" element={<CareerPresenter />} />
+          <Route path="/contact" element={<ContactPresenter />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/career/components/CareerPresenter.tsx
@@ -0,0 +1,7 @@
+export const CareerPresenter = () => {
+  return (
+    <div>
+      <h1>Career</h1>
+    </div>
+  );
+};
