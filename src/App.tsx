e0209d5cc6fab079388b918825c35bec073b2a75
mamenz752 <mamenz752@gmail.com>
2024-03-21 16:04:35 +0900

    fix: /状態でもコンテンツが表示されるよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePresenter } from "./components/home/components/HomePresenter";
 import { AboutPresenter } from "./components/about/components/AboutPresenter";
 import { CareerPresenter } from "./components/career/components/CareerPresenter";
 import { ContactPresenter } from "./components/contact/components/ContactPresenter";
@@ -13,6 +14,7 @@ function App() {
         <Header />
 
         <Routes>
+          <Route path="/" element={<HomePresenter />} />
           <Route path="/about" element={<AboutPresenter />} />
           <Route path="/career" element={<CareerPresenter />} />
           <Route path="/contact" element={<ContactPresenter />} />
