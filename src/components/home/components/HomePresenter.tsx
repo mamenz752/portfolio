01dd4904eaf949bf8647a9bbebc22f759a46fd34
mamenz752 <mamenz752@gmail.com>
2024-03-21 22:13:36 +0900

    feat: KVの実装

--- src/components/home/components/HomePresenter.tsx
+++ src/components/home/components/HomePresenter.tsx
@@ -1,24 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 export const HomePresenter = () => {
   return (
-    <div className="p-10 bg-mamenz-blue">
-      <div className="text-white">
-        <p>Let's</p>
-        <p>Make</p>
-        <p>Everyone</p>
-        <p>Smile</p>
-        <p>
-          By <span className="text-mamenz-yellow">Web</span>
-        </p>
-      </div>
-      <div className="text-white">
-        <p>
-          データサイエンスで
-          <span className="make-smile">みんなの笑顔を作ろう</span>
-        </p>
-      </div>
-      <div className="text-mamenz-yellow">
+    <div className="h-screen flex justify-center items-center flex-col gap-20 bg-mamenz-blue">
+      <div className="text-mamenz-yellow font-lexend font-extrabold tracking-widest text-8xl">
         <p>\(^_^)/</p>
+      </div>
+      <div className="flex justify-center items-center flex-col gap-2 font-lexend font-extrabold tracking-widest text-6xl text-white">
+        <p>Let's Make</p>
+        <p>Everyone Smile</p>
+        <p>
+          By <span className="text-mamenz-yellow">Internet</span>
+        </p>
+      </div>
+      <div className="font-zen-kaku font-bold text-xl text-white tracking-wider">
+        <p>サヤのポートフォリオサイトへようこそ！</p>
       </div>
     </div>
   );
