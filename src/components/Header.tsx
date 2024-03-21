9865fece9b8fe7fa8972c0ae84ef6d01567cb8d0
mamenz752 <mamenz752@gmail.com>
2024-03-21 15:32:22 +0900

    feat: headerの実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,28 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
-    <header className="">
-      <div>
-        <img src="" alt="logo" />
-      </div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+    <header className="flex items-center justify-around bg-mamenz-blue">
+      <Link to="/">
+        <div className="w-24">
+          <img src="./logo.png" alt="mamenz752のlogo" />
+        </div>
+      </Link>
+      <ul className="flex gap-6 text-xl tracking-widest text-white">
+        {/* TODO: liをHeaderLinkコンポーネント化 */}
+        <li className="hover:text-mamenz-pink">
+          <Link to="/">HOME</Link>
+        </li>
+        <li className="hover:text-mamenz-pink">
+          <Link to="/about">ABOUT</Link>
+        </li>
+        <li className="hover:text-mamenz-pink">
+          <Link to="/career">CAREER</Link>
+        </li>
+        <li className="hover:text-mamenz-pink">
+          <Link to="/contact">CONTACT</Link>
+        </li>
+      </ul>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        "noto-sans-jp": ["Noto Sans JP", "sans-serif"],
+      },
       colors: {
-        "mamenz-blue": "#000998",
+        "mamenz-blue": "#000980",
         "mamenz-green": "#8DFB21",
         "mamenz-yellow": "#FFF246",
         "mamenz-pink": "#FF0090",
+        "mamenz-bg-gray": "#EEEFFF",
       },
     },
   },
