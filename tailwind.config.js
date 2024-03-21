19b834894031f82ae459139fa278226449fabcfa
mamenz752 <mamenz752@gmail.com>
2024-03-21 14:59:42 +0900

    feat: tailwindcssのカラーパレット実装

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mamenz-blue": "#000998",
+        "mamenz-green": "#8DFB21",
+        "mamenz-yellow": "#FFF246",
+        "mamenz-pink": "#FF0090",
+      },
+    },
   },
   plugins: [],
 };
