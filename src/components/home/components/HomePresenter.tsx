982637086319bc86b82b1370f30dc64d11b77319
mamenz752 <mamenz752@gmail.com>
2024-05-06 18:28:49 +0900

    fix: KVのUIをデザイン通りに変更

--- src/components/home/components/HomePresenter.tsx
+++ src/components/home/components/HomePresenter.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
 export const HomePresenter = () => {
   return (
-    <div className="h-screen flex justify-center items-center flex-col gap-20 bg-mamenz-blue">
-      <div className="text-mamenz-yellow font-lexend font-extrabold tracking-widest text-8xl">
-        <p>\(^_^)/</p>
-      </div>
-      <div className="flex justify-center items-center flex-col gap-2 font-lexend font-extrabold tracking-widest text-6xl text-white">
-        <p>Let's Make</p>
-        <p>Everyone Smile</p>
-        <p>
-          By <span className="text-mamenz-yellow">Internet</span>
-        </p>
-      </div>
-      <div className="font-zen-kaku font-bold text-xl text-white tracking-wider">
-        <p>サヤのポートフォリオサイトへようこそ！</p>
+    <div className="h-screen flex items-center bg-mamenz-blue">
+      <div className="w-2/3 mx-auto">
+        <div className="flex justify-center items-start flex-col gap-2 font-lexend font-extrabold tracking-widest text-6xl text-white">
+          <p>Let's</p>
+          <p>Make</p>
+          <p>Everyone</p>
+          <p>Smile</p>
+          <p>
+            By <span className="text-mamenz-yellow">Internet</span>
+          </p>
+        </div>
+        <div className="mt-8 font-zen-kaku font-bold text-2xl text-white tracking-wider">
+          <p>サヤのポートフォリオサイトへようこそ！</p>
+        </div>
+        <div className="mt-8 flex justify-end text-mamenz-yellow font-lexend font-extrabold text-right tracking-widest text-6xl">
+          <p>\(^_^)/</p>
+        </div>
       </div>
     </div>
   );
