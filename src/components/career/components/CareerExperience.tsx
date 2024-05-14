c738c688fbea71206be603f0d6d1633548fc0a89
mamenz752 <mamenz752@gmail.com>
2024-05-15 01:00:30 +0900

    update: Careerの内容をアップデート

--- src/components/career/components/CareerExperience.tsx
+++ src/components/career/components/CareerExperience.tsx
@@ -23,7 +23,23 @@ const CareerExperience = () => {
                 習慣をゲーム感覚で身につけるアプリ「Rhythmate」を制作しました
               </li>
               <li>主に企画・UIデザイン・フロントエンドを担当しました</li>
-              <li>このハッカソンにおいて努力賞を受賞しました</li>
+              <li>
+                このハッカソンにおいて<b>努力賞</b>を受賞しました！
+              </li>
+            </ul>
+          </li>
+          <li className="flex flex-col gap-4">
+            <h2 className="text-lg font-bold">2024/05</h2>
+            <h1 className="text-xl">
+              <span className="bg-gradient-to-b from-transparent via-50% via-transparent to-mamenz-yellow">
+                技育博2024 vol.2 参加
+              </span>
+            </h1>
+            <ul className="ml-8 flex flex-col gap-2 font-normal list-disc">
+              <li>「Rhythmate」を約3ヶ月間継続開発しました</li>
+              <li>
+                企業賞として<b>GMOインターネットグループ賞</b>を受賞しました！
+              </li>
             </ul>
           </li>
         </ul>
