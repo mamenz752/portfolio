5b756ecd701cdeb504c32ea64a6f63397ff340cc
mamenz752 <mamenz752@gmail.com>
2024-05-15 01:10:38 +0900

    feat: Works切り替えボタンの実装

--- src/components/career/components/CareerWorks.tsx
+++ src/components/career/components/CareerWorks.tsx
@@ -7,7 +7,26 @@ const CareerWorks = () => {
       <div className="py-4">
         <Headings en="WORKS" ja="作品・実績" size="S" color="pink" />
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center gap-4">
+        <button className="w-[10%] h-24 flex justify-center items-center bg-mamenz-blue">
+          <svg
+            className="w-8 h-8 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m15 19-7-7 7-7"
+            />
+          </svg>
+        </button>
         <ul className="w-[80%]">
           <li>
             <div className="border-2 border-mamenz-blue">
@@ -63,6 +82,25 @@ const CareerWorks = () => {
             </ul>
           </li>
         </ul>
+        <button className="w-[10%] h-24 flex justify-center items-center bg-mamenz-blue">
+          <svg
+            className="w-8 h-8 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m9 5 7 7-7 7"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
