8ebc7a4445601788d4a183ec2064123edb8fcd53
mamenz752 <mamenz752@gmail.com>
2024-07-07 14:03:33 +0900

    feat: HomeにおけるStorybookの登録

---
+++ src/stories/home/HomeEmoji.stories.tsx
@@ -0,0 +1,12 @@
+import HomeEmoji from "@/components/home/HomeEmoji";
+import { Meta } from "@storybook/react";
+
+const meta: Meta<typeof HomeEmoji> = {
+  component: HomeEmoji,
+  title: "Home/Emoji",
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Emoji = {};
