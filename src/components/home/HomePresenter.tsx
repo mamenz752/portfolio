902f80dfdf8521456e7bb1feebce3ff3f2c3426f
mamenz752 <mamenz752@gmail.com>
2024-07-07 12:02:22 +0900

    refac: HomePresenterのdiv二重タグ修正

--- src/components/home/HomePresenter.tsx
+++ src/components/home/HomePresenter.tsx
@@ -5,12 +5,10 @@ import HomeWelcomeJp from "./HomeWelcomeJp";
 /* eslint-disable react/no-unescaped-entities */
 export const HomePresenter = () => {
   return (
-    <div className="h-screen flex items-center bg-mamenz-blue">
-      <div className="w-2/3 mx-auto">
-        <HomeWelcomeEn />
-        <HomeWelcomeJp />
-        <HomeEmoji />
-      </div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-mamenz-blue">
+      <HomeWelcomeEn />
+      <HomeWelcomeJp />
+      <HomeEmoji />
     </div>
   );
 };
