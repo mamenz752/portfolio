cd5459f8b6481a9ca5ec548a36b3e9e2fc716bb9
mamenz752 <mamenz752@gmail.com>
2024-07-07 12:14:49 +0900

    feat: md,smのレスポンシブ対応

--- src/components/home/HomeWelcomeEn.tsx
+++ src/components/home/HomeWelcomeEn.tsx
@@ -1,6 +1,6 @@
 const HomeWelcomeEn = () => {
   return (
-    <div className="flex justify-center items-start flex-col gap-2 font-lexend font-extrabold tracking-widest text-6xl text-white">
+    <div className="flex justify-center flex-col gap-2 font-lexend font-extrabold tracking-widest text-6xl text-white max-sm:font-bold max-sm:tracking-wide max-sm:text-3xl">
       <p>Let&apos;s</p>
       <p>Make</p>
       <p>Everyone</p>
--- src/components/home/HomeWelcomeJp.tsx
+++ src/components/home/HomeWelcomeJp.tsx
@@ -1,6 +1,6 @@
 const HomeWelcomeJp = () => {
   return (
-    <div className="mt-8 font-zen-kaku font-bold text-2xl text-white tracking-wider">
+    <div className="mt-8 font-zen-kaku font-bold text-2xl text-white tracking-wider max-sm:mt-4 max-sm:font-bold max-sm:tracking-wide max-sm:text-base">
       <p>サヤのポートフォリオサイトへようこそ！</p>
     </div>
   );
