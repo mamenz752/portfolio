16ba8416bba9e3e531b5f186f596f6b9e2c748b8
mamenz752 <mamenz752@gmail.com>
2024-07-10 22:11:29 +0900

    feat: HeaderのStorybook実装

---
+++ src/stories/common/HeaderLinkItem.stories.ts
@@ -0,0 +1,12 @@
+import { HeaderLinkItem } from "@/components/common/components/HeaderLinkItem";
+import { Meta } from "@storybook/react";
+
+const meta: Meta<typeof HeaderLinkItem> = {
+  component: HeaderLinkItem,
+  title: "Common/LinkItem",
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const LinkItem = {};
