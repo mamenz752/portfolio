dd8522af4b660b33f207018e3d9d4df9afd6f123
mamenz752 <mamenz752@gmail.com>
2024-08-12 17:51:05 +0900

    feat: Careerの装飾の追加実装

---
+++ src/components/career/components/CareerSquare.tsx
@@ -0,0 +1,12 @@
+const CareerSquare = () => {
+  return (
+    <div className="mt-8 mb-20 w-full h-full flex bg-mamenz-blue relative">
+      <div className="w-[100%] h-16 bg-mamenz-blue absolute top-0 left-0"></div>
+      <div className="w-12 h-12 bg-mamenz-pink absolute left-8 bottom-[-5.5rem] z-10"></div>
+      <div className="w-20 h-20 bg-mamenz-green absolute right-12 top-[-2.5rem] z-10"></div>
+      <div className="w-12 h-12 bg-mamenz-yellow absolute right-6 top-[-4rem] z-10"></div>
+    </div>
+  );
+};
+
+export default CareerSquare;
