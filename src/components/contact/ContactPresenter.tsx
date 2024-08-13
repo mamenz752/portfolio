19554a944ea99a914662cc446917afe3e5cfc619
mamenz752 <mamenz752@gmail.com>
2024-08-13 11:22:08 +0900

    update: /contactのコンテンツアップデート

--- src/components/contact/ContactPresenter.tsx
+++ src/components/contact/ContactPresenter.tsx
@@ -2,9 +2,14 @@ import { Headings } from "@/components/common/components/Headings";
 
 export const ContactPresenter = () => {
   return (
-    <div className="w-1/3 mx-auto my-10 flex justify-center items-start flex-col">
-      <Headings en="TECH LOG" ja="技術日報" size="N" color="" />
-      <p className="font-zen-kaku text-mamenz-blue">Coming Soon...</p>
+    <div className="w-1/3 max-lg:w-2/3 max-md:w-3/4 min-h-screen mx-auto my-10 flex items-start flex-col">
+      <Headings en="CONTACT" ja="お問い合わせ" size="N" color="" />
+      <p className="font-zen-kaku text-mamenz-blue">
+        現在は受け付けておりません
+      </p>
+      <div className="m-auto flex justify-center items-center text-mamenz-blue font-lexend font-extrabold tracking-widest text-6xl max-sm:mt-4 max-sm:font-bold max-sm:tracking-wide max-sm:text-4xl">
+        <p>m(_ _)m</p>
+      </div>
     </div>
   );
 };
