08081b53ff6e90a6d2b090d49f01a629d33689c2
mamenz752 <mamenz752@gmail.com>
2024-08-13 11:36:34 +0900

    fix: Hamburgerメニューの修正

--- src/components/common/components/HeaderLinkItem.tsx
+++ src/components/common/components/HeaderLinkItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const HeaderLinkItem: FC<Props> = ({ href, type }) => {
   return (
-    <li className="relative after:absolute after:top-1/2 after:left-[-10%] after:hover:w-[120%] after:hover:h-[2px] after:hover:bg-white after:hover:animate-animate-line">
+    <li className="relative after:absolute after:top-1/2 after:left-[-10%] after:hover:w-[120%] after:hover:h-[2px] after:hover:bg-white after:hover:animate-animate-line max-lg:after:hover:h-0">
       <Link to={href}>{type}</Link>
     </li>
   );
