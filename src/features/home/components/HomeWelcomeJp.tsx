f7a48987d8fbabd148c8f5452a04111455ced41c
mamenz752 <mamenz752@gmail.com>
2025-02-17 11:43:25 +0900

    refac: change export types in home components

--- src/features/home/components/HomeWelcomeJp.tsx
+++ src/features/home/components/HomeWelcomeJp.tsx
@@ -1,9 +1,7 @@
-const HomeWelcomeJp = () => {
+export const HomeWelcomeJp = () => {
   return (
     <div className="mt-8 font-zen-kaku font-bold text-2xl text-white tracking-wider max-sm:mt-4 max-sm:font-bold max-sm:tracking-wide max-sm:text-base">
       <p>サヤのポートフォリオサイトへようこそ！</p>
     </div>
   );
 };
-
-export default HomeWelcomeJp;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
-import HomeEmoji from "../features/home/components/HomeEmoji";
-import HomeWelcomeEn from "../features/home/components/HomeWelcomeEn";
-import HomeWelcomeJp from "../features/home/components/HomeWelcomeJp";
+import { HomeWelcomeEn } from "@/features/home/components/HomeWelcomeEn";
+import { HomeWelcomeJp } from "@/features/home/components/HomeWelcomeJp";
+import { HomeEmoji } from "@/features/home/components/HomeEmoji";
 
 /* eslint-disable react/no-unescaped-entities */
 const Home = () => {
