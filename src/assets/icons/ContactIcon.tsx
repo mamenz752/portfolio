ebc1cf112a6473d51b95c377cb22aeee08b5662a
mamenz752 <mamenz752@gmail.com>
2025-04-09 00:19:28 +0900

    feat: import fontawesome icon component

--- src/assets/icons/ContactIcon.tsx
+++ src/assets/icons/ContactIcon.tsx
@@ -1,11 +1,31 @@
 import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons/faXTwitter";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
 
 type Props = {
   type: string;
 };
 
 export const ContactIcon: FC<Props> = ({ type }) => {
+  const handleIcon = () => {
+    switch (type) {
+      case "x-twitter":
+        return faXTwitter;
+      case "github":
+        return faGithub;
+      case "instagram":
+        return faInstagram;
+      default:
+        return faXTwitter;
+    }
+  };
+
   return (
-    <i className={`fa-brands ${type} text-white text-4xl max-md:text-2xl`} />
+    <FontAwesomeIcon
+      icon={handleIcon()}
+      className="text-white text-4xl max-md:text-2xl"
+    />
   );
 };
